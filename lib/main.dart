4c6f07e62eb7b0be30ed9bca6c9935f3f29098f3
Chi Nghia <nghia520414@gmail.com>
2024-07-20 15:26:29 +0700

    Learn AppBar tittle.. navigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,33 @@ void main() {
       ),
       home: const MyHomePage(),
       routes: {
-        '/second': (context) => const SecondScreen(),
+        '/second': (context) =>  SecondScreen(),
       },
     ),
   );
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// taạo bottom navigationbar
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +44,7 @@ class MyHomePage extends StatelessWidget {
         leading: IconButton(
           icon: const Icon(Icons.home),
           onPressed: () {
-            // hành động cuủa icon
+            // Hành động của icon
           },
         ),
         title: const Center(
@@ -33,20 +52,75 @@ class MyHomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Chuyển đến Màn hình thứ hai'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-          },
-        ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: _onTabTapped,
       ),
     );
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Chuyển đến Màn hình thứ hai'),
+        onPressed: () {
+          Navigator.pushNamed(context, '/second');
+        },
+      ),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Search Screen'),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile Screen'),
+    );
+  }
+}
+
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
+   SecondScreen({super.key});
+
+  final List<String> nameUser = ['Nghĩa', 'Nam', 'hehe', 'hihi', 'Tuấn', 'Trà'];
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +131,16 @@ class SecondScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.green,
       ),
-      body: const Center(
-        child: Text('Đây là màn hình thứ hai'),
+      body: ListView.builder(
+        itemCount: nameUser.length,
+        itemBuilder: (context, index) {
+          final name = nameUser[index];
+          return ListTile(
+            leading: const Icon(Icons.person),
+            title: Text(name),
+            subtitle: Text('Thành viên thứ ${index + 1}'),
+          );
+        },
       ),
     );
   }
